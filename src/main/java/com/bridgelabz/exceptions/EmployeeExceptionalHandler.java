ca0d6221f1d3392d8203588f988d94050d70f07d
hp-pc <navyashree811@gmail.com>
2022-10-18 00:29:06 +0530

    Added : Provide user Friendly Error Response

---
+++ src/main/java/com/bridgelabz/exceptions/EmployeeExceptionalHandler.java
@@ -0,0 +1,30 @@
+package com.bridgelabz.exceptions;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.bridgelabz.dto.ResponseDto;
+
+@ControllerAdvice
+public class EmployeeExceptionalHandler {
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+
+	public ResponseEntity<ResponseDto> handleMethodArgumentNotValidException(
+			MethodArgumentNotValidException exception) {
+		List<ObjectError> errorList = exception.getBindingResult().getAllErrors();
+		List<String> errMesg = errorList.stream().map(objErr -> objErr.getDefaultMessage())
+				.collect(Collectors.toList());
+		ResponseDto responseDTO = new ResponseDto("Exception while processing REST Request", errMesg);
+
+		return new ResponseEntity<ResponseDto>(responseDTO, HttpStatus.BAD_REQUEST);
+
+	}
+
+}
