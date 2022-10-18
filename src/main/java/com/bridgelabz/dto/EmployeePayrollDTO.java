294b3c4483b39852b9fecdc5e379e2cf35c7f134
hp-pc <navyashree811@gmail.com>
2022-10-18 23:32:04 +0530

    Added : Ensure Validation is done

--- src/main/java/com/bridgelabz/dto/EmployeePayrollDTO.java
+++ src/main/java/com/bridgelabz/dto/EmployeePayrollDTO.java
@@ -3,9 +3,14 @@ package com.bridgelabz.dto;
 import java.util.List;
 
 import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PastOrPresent;
 import javax.validation.constraints.Pattern;
 
 import org.hibernate.validator.constraints.NotEmpty;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 import lombok.Data;
 
@@ -19,10 +24,21 @@ public class EmployeePayrollDTO {
 	@Min(value = 500, message = "Min wage should be more than 500")
 	public long salary;
 
+	@Pattern(regexp = "male|female", message = "Gender needs to be male or female")
 	public String gender;
+
+	@JsonFormat(pattern = "dd MM yyyy")
+	@NotNull(message = "startDate should Not be Empty")
+	@PastOrPresent(message = "startDate should be past or today date")
 	public String startDate;
+
+	@NotBlank(message = "Note cannot be Empty")
 	public String note;
+
+	@NotBlank(message = "ProfilePic cannot be Empty")
 	public String profilePic;
+
+	@NotNull(message = "department should Not be Empty")
 	public List<String> department;
 
 }
--- src/main/java/com/bridgelabz/exceptions/EmployeeExceptionalHandler.java
+++ src/main/java/com/bridgelabz/exceptions/EmployeeExceptionalHandler.java
@@ -5,6 +5,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -12,8 +13,25 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import com.bridgelabz.dto.ResponseDto;
 
+import lombok.extern.slf4j.Slf4j;
+
 @ControllerAdvice
+@Slf4j
 public class EmployeeExceptionalHandler {
+
+	private static final String message = "Exception while Processsing REST Request ";
+
+	@ExceptionHandler(HttpMessageNotReadableException.class)
+
+	public ResponseEntity<ResponseDto> handleHttpMessageNotReadableException(
+			HttpMessageNotReadableException exception) {
+
+		log.error("Invalid Data Formate", exception);
+		ResponseDto responseDTO = new ResponseDto(message, "Should have date in the Formate dd MM yyyy");
+		return new ResponseEntity<ResponseDto>(responseDTO, HttpStatus.BAD_REQUEST);
+
+	}
+
 	@ExceptionHandler(MethodArgumentNotValidException.class)
 
 	public ResponseEntity<ResponseDto> handleMethodArgumentNotValidException(
